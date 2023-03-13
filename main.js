3bf7c9fbd02999e43143c57ad4123a07ae187795
= <=>
2023-03-13 17:07:16 -0500

    addCarusel

--- main.js
+++ main.js
@@ -6,12 +6,12 @@
 //         shift: 5,
 //         padding: 5,
 //         numVisible: 3,
-//         indicators: true,
+// .        indicators: true,
 //         noWrap: true
 //     });
 // });
 
-
+let slideThumnail = 0;
 const navToggle = document.querySelector(".navbar-toggler")
 const navLink = document.querySelector(".navbar-nav")
 
@@ -19,10 +19,49 @@ navToggle.addEventListener("click", () => {
     navLink.classList.toggle("navbar-nav_visible")
 });
 
+let caruselItem = document.querySelector('.carousel-item');
+let galeria = document.querySelector('.galeria');
 
 
 
+const carusel = [
+    'img/control.jpg',
+    'img/control2.jpg',
+    'img/ps4Pro.jpg',
+    'img/ps4Pro2.jpg'
+];
+
+// function thumnail() {
+
+//     for (i = 0; i < carusel.length; i++) {
+//     caruselItem.style.backgroundImage = carusel[i];
+//     };
+
+//     slideThumnail++;
+
+//     if(slideThumnail > carusel.length) {slideThumnail = 1};
+//     caruselItem.style.backgroundImage = carusel[slideThumnail-1];
+
+//     setTimeout(thumnail,6000);
+// };
+// thumnail();
 
 
+console.log(carusel[0]);
 
 
+galeria.addEventListener('touchmove', () => {
+    // thumnail();
+    for (i = 0; i < carusel.length; i++) {
+    caruselItem.style.backgroundImage = `url(${carusel[i]})`;
+    };
+
+    slideThumnail++;
+
+    if(slideThumnail > carusel.length) {slideThumnail = 1};
+    caruselItem.style.backgroundImage = `url(${carusel[slideThumnail-1]})`;
+
+    // setTimeout(carusel,8000);
+    // caruselItem.classList.toggle(`url(${carusel[0]})`);
+    // caruselItem.style.backgroundImage = carusel;
+});
