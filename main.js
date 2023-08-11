ec20d58c99def3971504a800ee5d482536c24fbf
= <=>
2023-08-11 03:50:08 -0500

    v1.0.0.0

--- main.js
+++ main.js
@@ -12,6 +12,8 @@
 // });
 
 let slideThumnail = 0;
+let slideThumnail2 = 0;
+
 const navToggle = document.querySelector(".navbar-toggler")
 const navLink = document.querySelector(".navbar-nav")
 
@@ -25,24 +27,57 @@ let galeria = document.querySelector('.galeria');
 
 
 const carusel = [
-    'img/control.jpg',
-    'img/control23.jpg',
+    'img/newd.jpeg',
+    'img/OIP.jfif',
     'img/ps4Pro.jpg',
     'img/ps4Pro2.jpg'
 ];
 
 function thumnail() {
-
+    
     for (i = 0; i < carusel.length; i++) {
-    caruselItem.style.backgroundImage = `url(${carusel[i]})`;
+        caruselItem.style.backgroundImage = `url(${carusel[i]})`;
+        
     };
 
     slideThumnail++;
+    slideThumnail2++;
 
     if(slideThumnail > carusel.length) {slideThumnail = 1};
+    if(slideThumnail > carusel.length) {
+        slideThumnail2 = 1;
+         
+        
+    };
     caruselItem.style.backgroundImage = `url(${carusel[slideThumnail-1]})`;
+   // console.log(carusel[slideThumnail-1]);
+    if (carusel[slideThumnail-1] == "img/OIP.jfif") {
+            let info = "<p class='jsSlider'>Node.js, JavaScript, SQLite. Base de datos a un click.</p>";
+            let p = document.createElement("figcaption");
+            p = info;
+            // caruselItem = p;
+            
+            caruselItem.innerHTML=p;
 
+        } else if (carusel[slideThumnail-1] == "img/newd.jpeg") {
+            let p = document.createElement("figcaption");
+            let info1 = "<p class='jsSlider'>Reparación, mantenimiento, limpieza...</p>";
+            
+            p = info1;
+             caruselItem.innerHTML=p;
+        } else if (carusel[slideThumnail-1] == "img/ps4Pro.jpg") {
+            let p = document.createElement("figcaption");
+            let info2 = "<p class='jsSlider'>Renueva pasta térmica, PS4Pro.</p>";
+            
+            p = info2;
+             caruselItem.innerHTML=p;
+        }
+        else {
+            caruselItem.innerHTML="<p class='jsSlider'>Limpieza, consola de Video Juegos.</p>";
+        }
     setTimeout(thumnail, 8000);
+
+
     // caruselItem.classList.toggle(`url(${carusel[0]})`);
     // caruselItem.style.backgroundImage = carusel;
 };
@@ -66,7 +101,7 @@ caruselItem.addEventListener('touchstart', () => {
 const galeria2 = document.querySelector('.galeria2');
 const photos = document.getElementsByClassName('photo');
 const carousel = [
-    'img/lap01.jpeg',
+    'img/newg.jpeg',
     'img/lap02.jpeg',
     'img/lap03.jpeg',
     'img/lap04.jpeg',
