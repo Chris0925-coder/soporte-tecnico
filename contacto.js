89f37fcb5b54ddacf38f0e9a2feeea3af685d63e
Chris0925-coder = <christianguardia@outlook.es =>
2023-11-25 19:31:07 -0500

    123

--- contacto.js
+++ contacto.js
@@ -5,55 +5,46 @@ navToggle.addEventListener("click", () => {
     navLink.classList.toggle("navbar-nav_visible")
 });
 
-function grecaptcha() {
-    let grecaptcha = document.getElementsByClassName('g-recaptcha');
-    let response = grecaptcha.getResponse();
 
-    if(response.length == 0){
-        alert("Captcha no verificado");
-        return false;
-      event.preventDefault();
-    } else {
-      alert("Captcha verificado");
-      return true;
-    }
-  };
+
 
 
 
 
 const form = document.getElementById("formula");
-var email = document.querySelector("[name=email]").value;
-var control = document.querySelector("[name=control]").value;
+let email = document.querySelector("[name=email]").value;
+let control = document.querySelector("[name=control]").value;
 const btn = document.querySelector(".btn");
 // console.log(form);
 // console.log(email.length);
 // console.log(control.length);
 // console.log(btn);
 
+
+
 btn.addEventListener("click", () => {
-        email = document.querySelector("[name=email]").value;
-        control = document.querySelector("[name=control]").value;
-    if (email == "" || control == "") {
-        alert("Rellene los espacios");
-        if (email === "@") {
-            alert("Coloque un email valido")
-        }
-    }  else {
-        alert("¡GRACIAS! su comentario será leído...")
+    // event.preventDefault();
+    email = document.querySelector("[name=email]").value;
+    control = document.querySelector("[name=control]").value;
+    // mail = email.length;
+    console.log(email);
+
+
+    
+    if (!verificamail(email)) {
+        alert("Ingrese una direccion valida");
+        event.preventDefault();
+    } else {
+        // continue;
+        alert("¡Gracias por su comentario!");
+        // continue;
+        return;
     }
+
+    function verificamail(a) {
+       return a.length - a.replaceAll("@", "").length == 1;
+    };
 });
-
-
-
-// const enviarFormulario = (event) => {
-//     event.preventDefault();
-//     console.log(
-//         event.target.email.value,
-//         event.target.control.value
-//         );
-// };
-
 // form.addEventListener("submit", () => {
 //     enviarFormulario();
 //     fetch("https://api.mailjet.com/v3.1/send", requestOptions)
@@ -75,24 +66,12 @@ btn.addEventListener("click", () => {
 //           'sitekey' : '6Le1UBwpAAAAAHzagcQvjzbPGK2g7TvoJiLyNSO5'
 //         });
 
-let marck = document.getElementsByClassName("recaptcha-checkbox-checkmark");
-// onloadCallback;
 
-function miFuncion() {
-    // let grecaptcha = document.getElementsByClassName('g-recaptcha');
-    // var response = grecaptcha.getResponse();
 
-    if(marck){
-        alert("Captcha verificado");
-        btn.disabled = false;
-      
-    } else {
-        alert("Captcha no verificado");
-    }
-  }
-
-// function enabledSubmit(response) {
-//  btn.disabled = false;
+// function enabledSubmit(a) {
+//     if (email.focus != "") {
+//         a.style.display = "flex";
+//     };
 // };
 
-// enabledSubmit(response);
+// enabledSubmit(btn);
