d487659c36df2d7b4ac6741fc14ea00da6a9f73e
Chris0925-coder <christianguardia@outlook.es>
2024-01-10 16:02:33 -0500

    B

--- ads.js
+++ ads.js
@@ -1,12 +1,12 @@
-const ads = document.querySelector('.ads');
-let atOptions = {
-  'key' : '5115a1b458f6b8de84588523b50d8c38',
-  'format' : 'iframe',
-  'height' : 60,
-  'width' : 468,
-  'params' : {}
-};
-let doc = document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></scr' + 'ipt>');
+// const ads = document.querySelector('.ads');
+// let atOptions = {
+//   'key' : '5115a1b458f6b8de84588523b50d8c38',
+//   'format' : 'iframe',
+//   'height' : 60,
+//   'width' : 468,
+//   'params' : {}
+// };
+// let doc = document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></scr' + 'ipt>');
 
 
-ads.append(doc);
+// ads.append(atOptions + doc);
