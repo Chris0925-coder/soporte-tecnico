823775b7d8b1583b9e284f11662cc7e190db2666
Chris0925-coder <christianguardia@outlook.es>
2024-01-11 00:45:39 -0500

    123

--- ads.js
+++ ads.js
@@ -1,12 +1,12 @@
 // const ads = document.querySelector('.ads');
-// let atOptions = {
+// atOptions = {
 //   'key' : '5115a1b458f6b8de84588523b50d8c38',
 //   'format' : 'iframe',
 //   'height' : 60,
 //   'width' : 468,
 //   'params' : {}
 // };
-// let doc = document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></scr' + 'ipt>');
+// let doc = document.createTextNode('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></scr' + 'ipt>');
+// let a = document.createTextNode(atOptions);
 
-
-// ads.append(atOptions + doc);
+// ads.appendChild(a + doc);
