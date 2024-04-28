67c348ac5b7919f3329fee3a55684ebbea57f116
Chris0925-coder <christianguardia@outlook.es>
2024-04-28 16:44:48 -0500

    006

--- public/php/con_dbatlas.php
+++ public/php/con_dbatlas.php
@@ -9,12 +9,20 @@ $apiVersion = new ServerApi(ServerApi::V1);
 
 // Create a new client and connect to the server
 $client = new MongoDB\Client($uri), [], ['serverApi' => $apiVersion];
-
-try {
-    // Send a ping to confirm a successful connection
-    $client->selectDatabase('contacto')->command(['ping' => 1]);
-    echo "Pinged your deployment. You successfully connected to MongoDB!\n";
-} catch (Exception $e) {
-    printf($e->getMessage());
+if (isset($_POST['register'])){
+    if (strlen($_POST['email']) >= 1 && strlen($_POST['control']) >= 1) {
+        $email = trim($_POST['email']);
+        $control = trim($_POST['control']);
+    try {
+        // Send a ping to confirm a successful connection
+        $client->selectDatabase('contacto')->command(['ping' => 1]);
+        echo "Pinged your deployment. You successfully connected to MongoDB!\n";
+    } catch (Exception $e) {
+        printf($e->getMessage());
+    }
+} else {
+    ?>
+    <h3 class="bad">¡Complete los campos!</h3>
+    <?php
 }
 ?>
