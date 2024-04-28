695b4bcd82ec5c42c2361217543be022459dbee1
Chris0925-coder <christianguardia@outlook.es>
2024-04-28 17:01:37 -0500

    phpremove

--- public/php/comdb.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include("con_dbatlas.php");
-
-	// foreach ($conex->query('SELECT * FROM contacto') as $row)
-?>
--- public/php/con_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conex = mysqli_connect("localhost","id20634906_chris","Panama30!?","id20634906_chrisweb");
-
-?>
