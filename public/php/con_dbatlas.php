562514d5aab036042c4716c493fd01fa3239f71a
Chris0925-coder <christianguardia@outlook.es>
2024-04-28 15:04:57 -0500

    005

--- public/php/con_dbatlas.php
+++ public/php/con_dbatlas.php
@@ -1,14 +1,14 @@
 <?php
 use MongoDB\Driver\ServerApi;
 
-$uri = 'mongodb+srv://chris30:1987DarkSouls30!?@cluster0.jkdgz4b.mongodb.net/';
+$uri = 'mongodb+srv://chris09:Lrg7Fue3DqJ6DP5k@cluster0.jkdgz4b.mongodb.net/';
 
 // Set the version of the Stable API on the client
 $apiVersion = new ServerApi(ServerApi::V1);
 // , [], ['serverApi' => $apiVersion]
 
 // Create a new client and connect to the server
-$client = new MongoDB\Client($uri);
+$client = new MongoDB\Client($uri), [], ['serverApi' => $apiVersion];
 
 try {
     // Send a ping to confirm a successful connection
