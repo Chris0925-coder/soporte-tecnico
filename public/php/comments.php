48e25c47cb142bd8b09c96b30e08858749f45da1
Chris0925-coder <christianguardia@outlook.es>
2024-04-28 13:48:15 -0500

    001

--- public/php/comments.php
+++ public/php/comments.php
@@ -4,7 +4,7 @@
 	if (isset($client)) {
 		// function php_func() {
 			$consulta = "SELECT id, email, control, fech_reg FROM contacto";
-			$resultado = selectDatabase($uri, $consulta);
+			$resultado = selectDatabase($client, $consulta);
 			if ($resultado->num_rows > 0) {
 				while ($row =  $resultado->fetch_assoc()) {
 					echo 
--- public/php/delete.php
+++ public/php/delete.php
@@ -2,13 +2,13 @@
     include("con_dbatlas.php");
     // sql to delete a record
     $consulta = "DELETE FROM contacto WHERE id='".$_GET['id']."' ";
-    $resultado = mysqli_query($conex, $consulta);
+    $resultado = mysqli_query($client, $consulta);
     if ($resultado) {
        header("Location: comments.php");
     } else {
-        echo "Error deleting record: " . mysqli_error($conex);
+        echo "Error deleting record: " . mysqli_error($client);
     }
 
-    mysqli_close($conex);
+    mysqli_close($client);
 
 ?>
