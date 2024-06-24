a5af203034be84b13f44f0438fa7caea5773721d
Chris0925-coder <christianguardia@outlook.es>
2024-06-24 14:06:09 -0500

    005

--- public/javascript/ads.js
+++ public/javascript/ads.js
@@ -8,7 +8,7 @@ atOptions = {
   'width' : 468,
   'params' : {}
 };
-let doc = 'src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"';
+let doc = '<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></scr' + 'ipt>';
 // let a = document.createTextNode(atOptions);
 
 // ads.append(doc);
@@ -21,15 +21,15 @@ let doc = 'src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8
 //   'params' : {}
 // };
 let d = document.createElement('div');
-// let doc = `${atOptions}` + `<iframe 
+// let doc = `<iframe 
 //                 'key' : '5115a1b458f6b8de84588523b50d8c38',
 //               'format' : 'iframe',
 //               'height' : 60,
 //               'width' : 468,
 //               'params' : {}
-//                 src="https://www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></iframe>`;
+//                 // src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></iframe>`;
 
-d.innerHTML = atOptions + doc;
+d.innerHTML = `${atOptions}` + `${doc}`;
 ads.appendChild(d, cc);
 
 // ads = document.write(d,cc);
