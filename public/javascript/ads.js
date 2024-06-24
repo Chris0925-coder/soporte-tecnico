40e95b24436bbf38eb22f9d05008554da80287b4
Chris0925-coder <christianguardia@outlook.es>
2024-06-24 13:58:26 -0500

    004

--- public/javascript/ads.js
+++ public/javascript/ads.js
@@ -8,7 +8,7 @@ atOptions = {
   'width' : 468,
   'params' : {}
 };
-let doc = document.write('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></scr' + 'ipt>');
+let doc = 'src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"';
 // let a = document.createTextNode(atOptions);
 
 // ads.append(doc);
