77ae09d05e5038d8deeedc502838172d3b9133c4
Chris0925-coder <christianguardia@outlook.es>
2024-06-24 13:43:16 -0500

    00

--- public/javascript/ads.js
+++ public/javascript/ads.js
@@ -1,17 +1,18 @@
-// const ads = document.querySelector('.ads');
-// const ad = document.querySelector('.adA');
-// atOptions = {
-//   'key' : '5115a1b458f6b8de84588523b50d8c38',
-//   'format' : 'iframe',
-//   'height' : 60,
-//   'width' : 468,
-//   'params' : {}
-// };
-// let doc = ('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></scr' + 'ipt>');
+let ads = document.querySelector('.ads');
+let cc = ads.querySelector(".item:nth-child(2)");
+// let ad = document.querySelector('.adA');
+atOptions = {
+  'key' : '5115a1b458f6b8de84588523b50d8c38',
+  'format' : 'iframe',
+  'height' : 60,
+  'width' : 468,
+  'params' : {}
+};
+let doc = ('<scr' + 'ipt type="text/javascript" src="//www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></scr' + 'ipt>');
 // let a = document.createTextNode(atOptions);
 
 // ads.append(doc);
-// let cc = ads.querySelector(".item:nth-child(2)");
+
 // atOptions = {
 //   'key' : '5115a1b458f6b8de84588523b50d8c38',
 //   'format' : 'iframe',
@@ -19,16 +20,18 @@
 //   'width' : 468,
 //   'params' : {}
 // };
-// let d = document.createElement('div');
-// let doc = `<iframe 
-//                 key="5115a1b458f6b8de84588523b50d8c38"
-//                 title="Política de privacidad"
-//                 width="468"
-//                 height="60"
-//                 params : {}
+let d = document.createElement('div');
+// let doc = `${atOptions}` + `<iframe 
+//                 'key' : '5115a1b458f6b8de84588523b50d8c38',
+//               'format' : 'iframe',
+//               'height' : 60,
+//               'width' : 468,
+//               'params' : {}
 //                 src="https://www.profitablecreativeformat.com/5115a1b458f6b8de84588523b50d8c38/invoke.js"></iframe>`;
 
-// d.innerHTML = doc;
-// ad.append(d, cc);
+d.innerHTML = atOptions + document.write(doc);
+ads.appendChild(d, cc);
+
+// ads = document.write(d,cc);
 
 // ads.append(doc);
