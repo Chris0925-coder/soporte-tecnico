c4688be8639baa8e18e1430293c2c8ff8daed999
Chris0925-coder <christianguardia@outlook.es>
2024-08-23 13:32:29 -0500

    YT01

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -343,7 +343,7 @@ function onPlayerReady(event) {
 var done = false;
 function onPlayerStateChange(event) {
 if (event.data == YT.PlayerState.PLAYING && !done) {
-  setTimeout(stopVideo, 10000);
+  setTimeout(playVideo, 4000);
   done = true;
 }
 }
