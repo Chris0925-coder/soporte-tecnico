b56bc2663661665dd0d2a34156cb4d4494a6a8ce
Chris0925-coder = <christianguardia@outlook.es =>
2024-10-14 23:41:57 -0500

    003

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -387,7 +387,26 @@ let slideIndex = 0;
     });
 
 
+// window.twttr = (function(d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0],
+//       t = window.twttr || {};
+//     if (d.getElementById(id)) return t;
+//     js = d.createElement(s);
+//     js.id = id;
+//     js.src = "https://platform.twitter.com/widgets.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+  
+//     t._e = [];
+//     t.ready = function(f) {
+//       t._e.push(f);
+//     };
+//     return t;
+//     }(document, "script", "twitter-wjs"));
 
+// twttr.widgets.createTimeline({
+//     sourceType: 'profile',
+//     screenName 'ChrisWebDev30'
+// }, document.getElementById('container'));
 
 
 // fetch("https://www.youtube.com/embed/w3jrzlAijKA?si=N-ORTZq0Fxzaxrqi")
